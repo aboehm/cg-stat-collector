b122920db59131ed68719405562b55053c7349ee
Alexander Böhm <alxndr.boehm@gmail.com>
2016-06-14 18:11:19 +0200

    Restructured cgroup source

--- collector/sources/cgroup/__init__.py
+++ collector/sources/cgroup/__init__.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 # vim: ts=4 sw=4 noet
 
-import os
 from datetime import datetime
-import socket, hashlib
+import os, socket, hashlib
 from collector import Source, Document
+from collector.sources import compute_difference_over_dictonaries, field_converter_integer, field_converter_kilobyte, field_converter_nanosecond, field_converter_microsecond, field_converter_userhz
 
 class GroupException(Exception):
 	def __init__(self, cause, data=None):
@@ -31,45 +31,6 @@ class GroupExceptionInaccessiblePath(GroupException):
 	def __init__(self, path):
 		GroupException.__init__(self, "Path not accessible", path)
 
-
-def compute_difference_over_dictonaries(new, old, diffseconds = 0):
-	r = { }
-
-	for k in new:
-		if type(new[k]) == dict:
-			# jump to recursion
-
-			if k in old:
-				r[k] = compute_difference_over_dictonaries(new[k], old[k], diffseconds)
-			else:
-				r[k] = compute_difference_over_dictonaries(new[k], { }, diffseconds)
-
-		else:
-			# simple datatypes
-
-			if type(new[k]) == list:
-				r[k] = { "items": new[k] }
-
-				if k in old:
-					r[k].update({
-						"removed": list(set(new[k]).difference(set(old[k]))),
-						"added": list(set(old[k]).difference(set(new[k]))), 
-					})
-
-			elif type(new[k]) == float or type(new[k]) == int:
-				r[k] = { "absolute": new[k] }
-
-				if k in old:
-					r[k]["difference"] = new[k]-old[k]
-
-					if diffseconds != 0:
-						r[k]["difference_per_second"] = (new[k]-old[k])/diffseconds
-
-			else:
-				r[k] = new[k]
-
-	return r
-
 class CGroup(Source):
 	def __init__(self, name, gtype, path, source="Filesystem"):
 		Source.__init__(self, name)
@@ -77,9 +38,10 @@ class CGroup(Source):
 		self.path = os.path.abspath(path)
 		self.data = { }
 		self.last_data = { }
+		self.pids = { }
 		self.params = [
 			# name, filename, read function, simple value monotonic growing
-			("tasks", "tasks", self.read_param_number_array),
+			("tasks", "tasks", self.read_param_array, field_converter_integer),
 		]
 
 	def update(self):
@@ -88,8 +50,11 @@ class CGroup(Source):
 		self.last_data = self.data
 		self.data = { }
 
-		for param, filename, func in self.params:
-			self.data[param] = func(filename)
+		for param, filename, func, conv in self.params:
+			if param == "tasks":
+				self.pids = func(filename)
+
+			self.data[param] = func(filename, conv)
 
 	def build_data(self, timediff_sec):
 		return [compute_difference_over_dictonaries(self.data, self.last_data, timediff_sec)]
@@ -100,9 +65,10 @@ class CGroup(Source):
 		basedoc.update({
 			"name": self.name,
 			"path": self.path,
+			"pids": self.pids,
 		})
 
-		for d in self.build_data(td):
+		for d in self.build_data(self.get_timedelta()):
 			doc_data = basedoc.copy()
 			doc_data.update({ self.type: d })
 
@@ -125,7 +91,7 @@ class CGroup(Source):
 		if param.find("/") >= 0:
 			raise GroupExceptionBadPath(param)
 
-	def read_param(self, param):
+	def read_param(self, param, conv=None):
 		self.test_param(param)
 
 		p = self.path + "/" + param
@@ -143,16 +109,12 @@ class CGroup(Source):
 		if len(d) == 0:
 			return None
 
-		return d
-
-	def read_number_param(self, param):
-		v = self.read_param(param)
-		if v != None:
-			return int(v)
+		if conv != None:
+			return conv(d)
 		else:
-			return None
+			return d
 
-	def read_param_key_value(self, param):
+	def read_param_key_value(self, param, conv=None):
 		r = {}
 
 		d = self.read_param(param)
@@ -166,11 +128,14 @@ class CGroup(Source):
 			if len(j) != 2:
 				continue
 			else:
-				r[j[0]] = int(j[1])
+				if conv != None:
+					r[j[0]] = conv(j[1])
+				else:
+					r[j[0]] = j[1]
 
 		return r
 
-	def read_param_enumerated_number_array(self, param):
+	def read_param_enumerated_array(self, param, conv=None):
 		r = { }
 
 		d = self.read_param(param)
@@ -184,12 +149,16 @@ class CGroup(Source):
 			j = i.split(" ")
 			for k in j:
 				if k != "":
-					r["%i" % (idx)] = int(k)
+					if conv != None:
+						r["%i" % (idx)] = conv(k)
+					else:
+						r["%i" % (idx)] = int(k)
+
 					idx += 1
 
 		return r
 
-	def read_param_number_array(self, param):
+	def read_param_array(self, param, conv=None):
 		r = [] 
 
 		d = self.read_param(param)
@@ -203,7 +172,10 @@ class CGroup(Source):
 			j = i.split(" ")
 			for k in j:
 				if k != "":
-					r += [int(k)]
+					if conv != None:
+						r += [conv(k)]
+					else:
+						r += [int(k)]
 
 		return r
 
@@ -211,49 +183,43 @@ class CPUAccount(CGroup):
 	def __init__(self, name, path):
 		CGroup.__init__(self, name, self.__class__.__name__, path)
 		self.params += [
-			("usage", "cpuacct.usage", self.read_number_param),
-			("stat", "cpuacct.stat", self.read_param_key_value),
-			("usage_percpu", "cpuacct.usage_percpu", self.read_param_enumerated_number_array),
+			("usage", "cpuacct.usage", self.read_param, field_converter_nanosecond),
+			("stat", "cpuacct.stat", self.read_param_key_value, field_converter_userhz),
+			("usage_percpu", "cpuacct.usage_percpu", self.read_param_enumerated_array, field_converter_nanosecond),
 		]
 
 class CPU(CGroup):
 	def __init__(self, name, path):
 		CGroup.__init__(self, name, self.__class__.__name__, path)
 		self.params += [
-				("cfs_period_us", "cpu.cfs_period_us", self.read_number_param),
-				("stat", "cpu.stat", self.read_param_key_value),
-				("shares", "cpu.shares", self.read_number_param),
+				("cfs_period_us", "cpu.cfs_period_us", self.read_param, field_converter_microsecond),
+				("stat", "cpu.stat", self.read_param_key_value, field_converter_nanosecond),
+				("shares", "cpu.shares", self.read_param, field_converter_nanosecond),
 		]
 
 class Memory(CGroup):
 	def __init__(self, name, path):
 		CGroup.__init__(self, name, self.__class__.__name__, path)
 		self.params += [
-				("usage_in_bytes", "memory.usage_in_bytes", self.read_number_param),
-				("stat", "memory.stat", self.read_param_key_value),
+				("usage_in_bytes", "memory.usage_in_bytes", self.read_param, field_converter_integer),
+				("failcnt", "memory.failcnt", self.read_param, field_converter_integer),
+				("stat", "memory.stat", self.read_param_key_value, field_converter_integer),
 		]
 
 class Blkio(CGroup):
 	def __init__(self, name, path):
 		CGroup.__init__(self, name, self.__class__.__name__, path)
 		self.params += [
-				("io_service_bytes", "blkio.io_service_bytes", self.read_per_device_key_value),
-				("io_service_bytes_recursive", "blkio.io_service_bytes_recursive", self.read_per_device_key_value),
-				("io_service_time", "blkio.io_service_time", self.read_per_device_key_value),
-				("io_service_time_recursive", "blkio.io_service_time_recursive", self.read_per_device_key_value),
-				("io_merged", "blkio.io_merged", self.read_per_device_key_value),
-				("io_merged_recursive", "blkio.io_merged_recursive", self.read_per_device_key_value),
-				("io_wait_time", "blkio.io_wait_time", self.read_per_device_key_value),
-				("io_wait_time_recursive", "blkio.io_wait_time_recursive", self.read_per_device_key_value),
-				("io_queued", "blkio.io_queued", self.read_per_device_key_value),
-				("io_queued_recursive", "blkio.io_queued_recursive", self.read_per_device_key_value),
-
-#				("throttle_io_service_bytes", "blkio.throttle.io_service_bytes", self.read_per_device_key_value),
-#				("throttle_io_serviced", "blkio.throttle.io_serviced", self.read_per_device_key_value),
-#				("throttle_write_iops_device", "blkio.throttle.write_iops_device", self.read_per_device_key_value),
-#				("throttle_read_iops_device", "blkio.throttle.read_iops_device", self.read_per_device_key_value),
-#				("throttle_write_bps_device", "blkio.throttle.write_bps_device", self.read_per_device_key_value),
-#				("throttle_read_bps_device", "blkio.throttle.read_bps_device", self.read_per_device_key_value),
+				("io_service_bytes", "blkio.io_service_bytes", self.read_per_device_key_value, field_converter_integer),
+				("io_service_bytes_recursive", "blkio.io_service_bytes_recursive", self.read_per_device_key_value, field_converter_integer),
+				("io_service_time", "blkio.io_service_time", self.read_per_device_key_value, field_converter_nanosecond),
+				("io_service_time_recursive", "blkio.io_service_time_recursive", self.read_per_device_key_value, None),
+				("io_merged", "blkio.io_merged", self.read_per_device_key_value, field_converter_integer),
+				("io_merged_recursive", "blkio.io_merged_recursive", self.read_per_device_key_value, field_converter_integer),
+				("io_wait_time", "blkio.io_wait_time", self.read_per_device_key_value, field_converter_nanosecond),
+				("io_wait_time_recursive", "blkio.io_wait_time_recursive", self.read_per_device_key_value, field_converter_nanosecond),
+				("io_queued", "blkio.io_queued", self.read_per_device_key_value, field_converter_integer),
+				("io_queued_recursive", "blkio.io_queued_recursive", self.read_per_device_key_value, field_converter_integer),
 		]
 
 	def build_data(self, timediff_sec):
@@ -294,7 +260,7 @@ class Blkio(CGroup):
 
 		return r.values()
 
-	def read_per_device_key_value(self, param):
+	def read_per_device_key_value(self, param, conv=None):
 		r = { }
 
 		d = self.read_param(param)
@@ -341,6 +307,62 @@ class Blkio(CGroup):
 		else:
 			return majmin
 
+class CGroupFilesystem(Source):
+	def __init__(self, mount_point):
+		Source.__init__(self, "CGroupFilesystem")
+		self.mount_point = mount_point
+		self.controler = ["blkio", "cpu", "cpuacct", "memory"]
+		self.groups = { }
+
+	def enumerate_groups(self, controler):
+		r = []
+
+		search_path = "%s/%s" % (self.mount_point, controler)
+		for dirname, directories, files in os.walk(search_path):
+			cg = dirname[len(search_path):]
+			if cg == "":
+				cg = "/"
+
+			r += [(cg, dirname, controler)]
+
+		return r
+
+	def update(self):
+		cgs = []
+
+		for controler in self.controler:
+			cgs += self.enumerate_groups(controler)
+			if not controler in self.groups:
+				self.groups[controler] = { }
+
+		for name, path, controler in cgs:
+			if not name in self.groups[controler]:
+				if controler == "cpu":
+					self.groups[controler][name] = CPU(name, path)
+
+				elif controler == "cpuacct":
+					self.groups[controler][name] = CPUAccount(name, path)
+
+				elif controler == "memory":
+					self.groups[controler][name] = Memory(name, path)
+				
+				elif controler == "blkio":
+					self.groups[controler][name] = Blkio(name, path)
+
+				else:
+					continue
+
+			self.groups[controler][name].update()
+
+	def docs(self):
+		self.update()
+
+		docs = []
+		for controler in self.groups:
+			for name in self.groups[controler]:
+				docs += self.groups[controler][name].docs()
+
+		return docs
 
 
 
