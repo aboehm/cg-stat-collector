2309e03b1110d18a0351ed4779d465346f20a6b4
Alexander Böhm <alxndr.boehm@gmail.com>
2017-03-10 11:18:44 +0100

    Add license

--- cg-stat-collector.py
+++ cg-stat-collector.py
@@ -1,6 +1,21 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
-# vim: ts=4 sw=4 noet
+#
+# cg-stat-collector, a OS statistics collector. 
+# Copyright (C) 2017, Alexander Böhm <alxndr.boehm@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
 
 from collector import Collector
 from collector.sources.cgroup import CGroupFilesystem
@@ -202,3 +217,5 @@ Example:
 		else:
 			time.sleep(args.interval)
 
+# -*- coding: utf-8 -*-
+# vim: ts=4 sw=4 noet
